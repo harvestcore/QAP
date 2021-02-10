daa863badd5b8fda215612c786c91b3255a3805e
harvestcore <aagomezies@gmail.com>
2021-02-10 23:23:52 +0100

    change main to use tai256c

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from classes.population import Population
 
 if __name__ == '__main__':
-    p = Population(database='databases/nug12.dat', generations=50)
+    p = Population(database='databases/tai256c.dat', generations=50)
     p.run()
     print(p)
 
-    p1 = Population(database='databases/nug12.dat', generations=50, variant='lamarckian')
+    p1 = Population(database='databases/tai256c.dat', generations=50, variant='lamarckian')
     p1.run()
     print(p1)
 
-    p2 = Population(database='databases/nug12.dat', generations=50, variant='baldwinian')
+    p2 = Population(database='databases/tai256c.dat', generations=50, variant='baldwinian')
     p2.run()
     print(p2)
 
