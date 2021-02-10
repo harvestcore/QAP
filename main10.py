9296ccce54ea9238c0a93d03e4bf7e23a8812eb5
harvestcore <aagomezies@gmail.com>
2021-02-10 23:28:58 +0100

    add mains

---
+++ main10.py
@@ -0,0 +1,18 @@
+from classes.population import Population
+
+if __name__ == '__main__':
+    p = Population(database='databases/tai256c.dat', generations=10)
+    p.run()
+    print(p)
+
+    p1 = Population(database='databases/tai256c.dat', generations=10, variant='lamarckian')
+    p1.run()
+    print(p1)
+
+    p2 = Population(database='databases/tai256c.dat', generations=10, variant='baldwinian')
+    p2.run()
+    print(p2)
+
+# print(len(p.distances))
+# print(p.distances)
+# print(p.flows)
