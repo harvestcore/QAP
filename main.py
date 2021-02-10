8bbe615d543e4d06d4f5fe19a8eacb147594af1f
harvestcore <aagomezies@gmail.com>
2021-02-10 23:20:38 +0100

    Fix main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from classes.population import Population
 
-p = Population(database='databases/bur26a.dat', generations=100)
-p.run()
-print(p)
+if __name__ == '__main__':
+    p = Population(database='databases/nug12.dat', generations=50)
+    p.run()
+    print(p)
 
-p1 = Population(database='databases/bur26a.dat', generations=100, variant='lamarckian')
-p1.run()
-print(p1)
+    p1 = Population(database='databases/nug12.dat', generations=50, variant='lamarckian')
+    p1.run()
+    print(p1)
 
-p2 = Population(database='databases/bur26a.dat', generations=100, variant='baldwinian')
-p2.run()
-print(p2)
+    p2 = Population(database='databases/nug12.dat', generations=50, variant='baldwinian')
+    p2.run()
+    print(p2)
 
 # print(len(p.distances))
 # print(p.distances)
